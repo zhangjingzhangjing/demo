373d2c9d3cd30fe80d6093b77b1dcf4bb583f3bc
张晶 <1023517599@qq.com>
2020-08-13 11:00:53 +0800

    ggg

--- src/main/java/com/czxy/demo222.java
+++ src/main/java/com/czxy/demo222.java
@@ -1,7 +1,11 @@
 package com.czxy;
 
+
+
 public class demo222 {
     public static void main(String[] args) {
+        System.out.println(22222);
+        System.out.println(22222);
         System.out.println(22222);
     }
 }
