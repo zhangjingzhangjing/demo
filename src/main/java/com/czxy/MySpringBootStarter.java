e91bad37a34dba2ba19ecb3c69bbdd5539686976
张晶 <1023517599@qq.com>
2020-08-13 09:51:50 +0800

    测试

---
+++ src/main/java/com/czxy/MySpringBootStarter.java
@@ -0,0 +1,15 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication
+public class MySpringBootStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootStarter.class,args);
+    }
+
+}
