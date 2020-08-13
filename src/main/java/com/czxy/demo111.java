868a0f344909eab0678f5d7fce3ea95a54bdd303
朱星奇 <z15751601660@163.com>
2020-08-13 10:41:29 +0800

    修改java22

--- src/main/java/com/czxy/demo111.java
+++ src/main/java/com/czxy/demo111.java
@@ -4,6 +4,6 @@ public class demo111 {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("小晶晶");
-
+        System.out.println("211111");
     }
 }
