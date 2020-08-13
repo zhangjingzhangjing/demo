59854b1acf92bb149141eef52468763946846b0a
朱星奇 <z15751601660@163.com>
2020-08-13 10:32:57 +0800

    新增java

---
+++ src/main/java/com/czxy/demo111.java
@@ -0,0 +1,9 @@
+package com.czxy;
+
+public class demo111 {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("bbb");
+        System.out.println("ccc");
+    }
+}
