25bbaded8bedcba127330d4befbd47e47d2681ca
张晶 <1023517599@qq.com>
2020-08-13 11:04:31 +0800

    ggg

--- src/main/java/com/czxy/demo111.java
+++ src/main/java/com/czxy/demo111.java
@@ -4,6 +4,8 @@ public class demo111 {
     public static void main(String[] args) {
         System.out.println("aa");
         System.out.println("aaa");
-
+    }
+    public void run1(){
+        System.out.println("d");
     }
 }
